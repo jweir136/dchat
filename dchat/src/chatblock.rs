64d9babbb2b0497223a9e35f453bcc2ffb03e418
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-04 13:48:31 -0500

    add Chatblock test1

--- dchat/src/chatblock.rs
+++ dchat/src/chatblock.rs
@@ -42,3 +42,17 @@ impl Block for Chatblock {
         sha256_hash(format!("{}{}{}{:?}", &self.to, &self.from, &self.msg, &self.time))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_block_test() {
+        let block = Chatblock::new("Jake".to_string(), "Ryan".to_string(), "Hi".to_string());
+
+        assert_eq!(block.to(), "Jake".to_string());
+        assert_eq!(block.from(), "Ryan".to_string());
+        assert_eq!(block.msg(), "Hi".to_string());
+    }
+}
